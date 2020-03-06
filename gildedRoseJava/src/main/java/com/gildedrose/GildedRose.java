61d071bd094a87c93a39e7381d71b203279c2450
Mr abdo <Mr abdo@Mrabdo-PC>
2020-03-06 18:04:15 +0100

    refractoring By elalaoui abdellah

--- gildedRoseJava/src/main/java/com/gildedrose/GildedRose.java
+++ gildedRoseJava/src/main/java/com/gildedrose/GildedRose.java
@@ -6,30 +6,33 @@ class GildedRose {
 	public GildedRose(Item[] items) {
 		this.items = items;
 	}
+
 // the main method for updating both the quality and the sellIn for each product
 	public void updateQuality() {
 		for (int i = 0; i < items.length; i++) {
-			switch(items[i].name) {
+			switch (items[i].name) {
 			case "Aged Brie":
-				UpdateAgedBrie(i);break;
+				UpdateAgedBrie(i);
+				break;
 			case "Backstage passes to a TAFKAL80ETC concert":
-				UpdateBackstage(i);break;
-				
+				UpdateBackstage(i);
+				break;
+
 			case "Sulfuras, Hand of Ragnaros":
-				UpdateSulfurase(i);break;
-				
-			default :
-				UpdateOther(i);break;	
-				
+				//nothing to do because they always stable no update
+				break;
+
+			default:
+				UpdateOther(i);
+				break;
+
 			}
 		}
-			
-	
+
 	}
 
-	
-	//  below you will find the method for each  product 
-	
+	// below you will find the method for each product
+
 	public void UpdateAgedBrie(int i) {
 		if (items[i].quality > 49) {
 			items[i].sellIn--;
@@ -41,39 +44,40 @@ class GildedRose {
 	}
 
 	public void UpdateBackstage(int i) {
-		if (items[i].quality > 49 && items[i].sellIn > -1) {
 
-			items[i].sellIn--;
-		} else if (items[i].sellIn < 0) {
+		if (items[i].sellIn > 0) {
+
+			if (items[i].sellIn < 6 && items[i].quality < 48) {
+				items[i].quality += 3;
+				items[i].sellIn--;
+			} else if (items[i].sellIn < 11 && items[i].quality < 49) {
+				items[i].quality += 2;
+				items[i].sellIn--;
+			} else if (items[i].quality < 50) {
+				items[i].quality++;
+				items[i].sellIn--;
+
+			} else {
+				items[i].sellIn--;
+			}
+		} 
+		//if the backstage expire the quality automatically zero
+		else {
 			items[i].quality = 0;
-			items[i].sellIn--;
-		} else if (items[i].sellIn < 6 && items[i].quality < 50) {
-			items[i].quality += 3;
-			items[i].sellIn--;
-		} else if (items[i].sellIn < 11 && items[i].quality < 50) {
-			items[i].quality += 2;
-			items[i].sellIn--;
-		} else {
-			items[i].quality++;
 			items[i].sellIn--;
 		}
 
 	}
 
-	public void UpdateSulfurase(int i) {
-
-		//items[i].sellIn--;
-
-	}
+	
 
 	public void UpdateOther(int i) {
-		if (items[i].quality==0) {
+		if (items[i].quality == 0) {
 			items[i].sellIn--;
-		}else {
+		} else {
 			items[i].quality--;
 			items[i].sellIn--;
 		}
-		
-		
+
 	}
 }
